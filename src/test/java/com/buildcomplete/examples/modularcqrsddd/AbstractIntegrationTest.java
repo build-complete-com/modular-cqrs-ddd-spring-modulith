027940503c22befeaf695b8be56092e90cfa44c5
Mateusz Radziszewski <mtsz.radziszewski@gmail.com>
2024-06-16 11:16:07 +0200

    feat: Do not use Testcontainers JUnit integration - does not work with @ApplicationModuleTest

--- src/test/java/com/buildcomplete/examples/modularcqrsddd/AbstractIntegrationTest.java
+++ src/test/java/com/buildcomplete/examples/modularcqrsddd/AbstractIntegrationTest.java
@@ -6,6 +6,7 @@ import static org.testcontainers.containers.localstack.LocalStackContainer.Servi
 import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.Map;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.BeforeAll;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
@@ -15,23 +16,18 @@ import org.testcontainers.containers.MariaDBContainer;
 import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.containers.localstack.LocalStackContainer;
 import org.testcontainers.containers.wait.strategy.Wait;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.MountableFile;
 
 @SpringBootTest
-@Testcontainers
-class AbstractIntegrationTest {
+public class AbstractIntegrationTest {
   private static final int MONGO_DB_CONTAINER_PORT = 27017;
   private static final int LOCAL_STACK_CONTAINER_PORT = 4566;
   private static final int JSON_SERVER_CONTAINER_PORT = 80;
 
-  @Container
   static final MongoDBContainer MONGO_DB_CONTAINER = new MongoDBContainer("mongo:7.0")
       .withExposedPorts(MONGO_DB_CONTAINER_PORT)
       .waitingFor(Wait.forLogMessage(".*mongod startup complete.*", 1));
 
-  @Container
   static final LocalStackContainer LOCAL_STACK_CONTAINER = new LocalStackContainer("3.4.0")
       .withServices(SQS, SNS)
       .withExposedPorts(LOCAL_STACK_CONTAINER_PORT)
@@ -45,18 +41,23 @@ class AbstractIntegrationTest {
           "AWS_ENDPOINT", "http://localhost:" + LOCAL_STACK_CONTAINER_PORT
       ));
 
-  @Container
   static final MariaDBContainer MARIA_DB_CONTAINER = new MariaDBContainer("mariadb:10.3.6");
 
-  @Container
   static final GenericContainer JSON_SERVER = new GenericContainer("clue/json-server")
       .withExposedPorts(JSON_SERVER_CONTAINER_PORT)
       .withCopyFileToContainer(MountableFile.forHostPath(Paths.get("scripts/jsonserver/db.json")), "/data/db.json");
 
+  static {
+    Stream.of(MONGO_DB_CONTAINER, LOCAL_STACK_CONTAINER, MARIA_DB_CONTAINER).parallel().forEach(GenericContainer::start);
+    Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+      Stream.of(MONGO_DB_CONTAINER, LOCAL_STACK_CONTAINER, MARIA_DB_CONTAINER).parallel().forEach(GenericContainer::stop);
+    }));
+  }
+
   @DynamicPropertySource
   static void overrideProperties(DynamicPropertyRegistry registry) {
     registry.add("mongodb.connectionString", () -> MONGO_DB_CONTAINER.getConnectionString());
-    registry.add("spring.cloud.aws.endpoint", () -> LOCAL_STACK_CONTAINER.getEndpoint().toString());
+//    registry.add("spring.cloud.aws.endpoint", () -> LOCAL_STACK_CONTAINER.getEndpoint().toString());
     registry.add("spring.datasource.url", () -> MARIA_DB_CONTAINER.getJdbcUrl());
     registry.add("spring.datasource.username", () -> MARIA_DB_CONTAINER.getUsername());
     registry.add("spring.datasource.password", () -> MARIA_DB_CONTAINER.getPassword());
