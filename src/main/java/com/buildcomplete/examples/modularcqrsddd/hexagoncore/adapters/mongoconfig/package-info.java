e12cce60566759dfcc45fe78c87c96b22670c696
Mateusz Radziszewski <mtsz.radziszewski@gmail.com>
2024-07-03 09:32:52 +0200

    chore: remove application module annotation from mongo package

--- src/main/java/com/buildcomplete/examples/modularcqrsddd/hexagoncore/adapters/mongoconfig/package-info.java
+++
@@ -1,7 +0,0 @@
-@ApplicationModule(
-    displayName = "MongoDB Connection Configuration",
-    allowedDependencies = {}
-)
-package com.buildcomplete.examples.modularcqrsddd.hexagoncore.adapters.mongoconfig;
-
-import org.springframework.modulith.ApplicationModule;
